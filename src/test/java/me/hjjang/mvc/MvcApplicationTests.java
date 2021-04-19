0b7bcc15b7047e24e68c283348026d049cf1fc8a
HyungjuJang <dacapolife87@gmail.com>
2021-04-19 23:42:28 +0900

    feat : 기본 프로젝트파일 커밋

---
+++ src/test/java/me/hjjang/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hjjang.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
